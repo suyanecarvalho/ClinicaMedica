f21c53ec85bdd38a17eee808c40d4c46b11801a0
Suyane <suyanecarvalhof@gmail.com>
2023-06-21 01:57:06 -0300

    adicionando Tela inicial

---
+++ src/main/java/com/example/clinicamed/InicialController.java
@@ -0,0 +1,25 @@
+package com.example.clinicamed;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class InicialController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
